9270603fd9a3e4dac7fa1840f0330c65eb1a5807
SparkyT <tim.sparks+github@gmail.com>
2013-04-25 10:46:19 +0200

    Update OrgModePhases.js

--- OrgModePhases.js
+++ OrgModePhases.js
@@ -85,6 +85,7 @@ var tsFargo = {
 
   	myMsgDiv.hide();
   	myMsg.html(msgTxt);
+  	console.log('FI'+fadeInTime+' FO'+fadeOutTime);
   	myMsgDiv.fadeIn(fadeInTime,function(){
   		if(fadeOutTime==-1){
   			myMsgDiv.fadeOut(fadeOutTime);
