d67782c5b34bd64be9130b7bec92e5ec97994372
SparkyT <tim.sparks+github@gmail.com>
2013-05-25 20:40:46 +0200

    Update OrgModePhases.js

--- OrgModePhases.js
+++ OrgModePhases.js
@@ -106,7 +106,7 @@ var tsFargo = {
 					if(tab.flEditable && (tab.url!==undefined)){
 						confirmDialog ("Create a public link for the \""+tabTitle+"\" file?",function(){
 							vendor.createSharedUrl(tabUrl, function(publicUrl){
-								msgMe(publicUrl);
+								tsFargo.msgMe(publicUrl);
 								});
 							});
 						}
