7efa21384b3ad0d065c00cd72e0ef6a689bb947d
SparkyT <tim.sparks+github@gmail.com>
2013-05-30 00:29:42 +0200

    Update OrgModePhases.js
    Comments and cleanup

--- OrgModePhases.js
+++ OrgModePhases.js
@@ -1,72 +1,17 @@
-// NOTE: This currently only works in text mode.
-
-// NOTE: Labels can be expanded here. I imagine labels might have more intermediate phases like PENDING or WAITING.
-// I've included spaces in the labels in case you want to use other delimeters without changing the code
-var labels = ['TODO ', 'DONE '];
-
-var labelsRegex = new RegExp('^(' + labels.join('|') + ')');
-
-$(document).bind('keydown', function(event) {
-    // Bail if not in text mode
-    if($('.textMode').length == 0) {
-	return -1;
-    }
-    var theKey = event.which;
-    // Since this will run on every keydown, I'm doing my best to make sure this is as performant as possible by excluding as many cases as I can outright
-    // Is the shift key held down?
-    if(event.shiftKey && !event.ctrlKey) {
-	// Is this an arrow key?
-        if(theKey === 37 || theKey === 39) {
-	    var currentElement = $('div.active .concord-cursor > div > div');
-	    var currentText = currentElement.text();
-	    
-	    //Check which we're changing, priorities or labels
-	    if(theKey === 37 || theKey === 39) {
-		//Labels stuff
-		// Is there no label at all?
-		if(!labelsRegex.test(currentText)) {
-		    if(theKey === 37) {
-			currentElement.text(labels[labels.length - 1] + currentText);
-		    } else {
-			currentElement.text(labels[0] + currentText);
-		    }
-		} else {
-		    //Find the label and increment/decrement
-		    var parsedLabel = labelsRegex.exec(currentText)[0];
-		    var currentLabelIndex = labels.indexOf(parsedLabel);
-		    if(theKey === 37) {
-			//Left Arrow
-			if(currentLabelIndex - 1 < 0) {
-			    currentElement.text(currentText.replace(labelsRegex, ''));
-			} else {
-			    currentElement.text(currentText.replace(labelsRegex, labels[currentLabelIndex - 1]));
-			}
-		    } else {
-			// Right Arrow
-			if(currentLabelIndex + 1 >= labels.length) {
-			    currentElement.text(currentText.replace(labelsRegex, ''));
-			} else {
-			    currentElement.text(currentText.replace(labelsRegex, labels[currentLabelIndex + 1]));
-			}
-		    }
-		}
-	    }
-        } 
-    }
-});
+// Thanks to https://github.com/Remixer96/FargoScripts for inspiration
 
 
-
-
-
-var myFM2Word = $('#tsFargo2Word');
+// Find our menu item and remove it if exists, then add it back in 
+//  TODO there must be a better way to deal with this!
+var myFM2Word = $('#tsFargo2HTMLList');
 if(myFM2Word.length === 0){
 }
 else {
 	myFM2Word.remove();	
 	$('#tsFargoDivider').remove();
 }
-myFM2Word = $('<li id="tsFargoDivider" class="divider"></li><li id="tsFargo2Word"><a onclick="tsFargo.tsFS();">* Export to Word</a><a id="idtsFargo2WordLink" href="#" target="_blank"></a></li>').appendTo('#idFileMenu ul.dropdown-menu');    
+// Add the divider and Export to HTML list
+myFM2Word = $('<li id="tsFargoDivider" class="divider"></li><li id="tsFargo2HTMLList"><a onclick="tsFargo.tsF2HTMLList();">* Export to HTML list</a></li>').appendTo('#idFileMenu ul.dropdown-menu');    
 
 
 var tsFargo = {
@@ -113,29 +58,22 @@ var tsFargo = {
   	});
  
   },
-  tsFS:				function fileShare (){
+  tsF2HTMLList:				function file2HTMLList (){
 					var tab = smallTabs.getActiveTab ();
 					var tabTitle = tab.getTitle();
 					var tabUrl = tab.url;
-					// direct from fargo.io;
+					// direct from fargo.io, tweked to remove blocking of popups ;
+					//	open window first and retain reference, then set location in callback; 
 					var tab = smallTabs.getActiveTab ();
-					var tsFSURL='';
+					//	TODO - Move to proper server when practical, thanks to Trinity URC admin for the use (that's me)
 					var baseurl = "http://www.trinity-urc.org.uk/fargo/xml.php?fargoOPML=";
 					var w = window.open( baseurl + 'redirecting' );
 					if (tab.type == "watchedOutline") {
 						w.location =baseurl + encodeURIComponent (tab.url);
-						//window.open (baseurl + encodeURIComponent (tab.url),'_blank');
-						//$('#idtsFargo2WordLink').attr('href',baseurl + encodeURIComponent (tab.url));
-						//$('#idtsFargo2WordLink').click();
-						//$('#idtsFargo2WordLink').attr('href','#');
 						}
 					else {
 						vendor.createSharedUrl (tab.url, function (publicUrl) {
 							w.location = baseurl + encodeURIComponent (publicUrl);
-							//window.open (baseurl + encodeURIComponent (publicUrl),'_blank');
-							//$('#idtsFargo2WordLink').attr('href',baseurl + encodeURIComponent (publicUrl));
-							//$('#idtsFargo2WordLink').click();
-							//$('#idtsFargo2WordLink').attr('href','#');
 							},
 							function () {
 								alertDialog ("Can't open the outline \"" + tab.title + "\" in Taco Pie."); 
@@ -145,15 +83,4 @@ var tsFargo = {
 						}
 											
   				},	
-/*						
-					if(tab.flEditable && (tab.url!==undefined)){
-						confirmDialog ("Create a public link for the \""+tabTitle+"\" file?",function(){
-							vendor.createSharedUrl(tabUrl, function(publicUrl){
-								//tsFargo.msgMe(publicUrl,600,-1);
-								window.open("http://www.trinity-urc.org.uk/fargo/xml.php?fargoOPML=" + publicUrl,'_blank');
-								});
-							});
-						}
-					}
-*/
 }
